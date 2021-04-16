e1c6428a6c2e630c051a80f491f69198d66b543a
Jessica Medeiros <jessicamedeirosp96@yahoo.com.br>
2021-04-16 09:39:21 -0300

    adding component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
+import { Header } from './components/Header/index';
 import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className="App">
-     <h1>Hello World</h1>
-     <GlobalStyle />
-    </div>
+    <>
+      <Header />
+      <h1>Hello World</h1>
+      <GlobalStyle />
+    </>
   );
 }
